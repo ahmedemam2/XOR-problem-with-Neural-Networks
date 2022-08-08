7101aeb597643aba30f5a614afc9da1bf5091e32
ahmedemam2 <88343933+ahmedemam2@users.noreply.github.com>
2022-08-08 06:41:34 -0700

    fixed inputs

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import ANN
 from random import randint
 def main():
-
-    trainingdataXOR = [0,0,0,0,1,1,1,0,1,1,1,0]
     nn = ANN.NN(2, 2, 1)
-    for n in range(10000):
-        r = randint(0,3)
-        nn.train([trainingdataXOR[r],trainingdataXOR[r+1]], trainingdataXOR[r+2])
-
+    for n in range (250):
+          nn.train([0,0],0)
     Guess = nn.Predict([0,0])
     print(Guess)
-
+    for n in range (250):
+          nn.train([0,1],1)
     Guess = nn.Predict([0,1])
     print(Guess)
-    Guess = nn.Predict([1,0])
-    print(Guess)
+    for n in range (250):
+          nn.train([1,1],0)
     Guess = nn.Predict([1,1])
     print(Guess)
+    for n in range (250):
+          nn.train([1,0],1)
+    Guess = nn.Predict([1,0])
+    print(Guess)
+
 
 
 main()
